64cd15c8f4b5660e352e92bbea6c2ecef2aa8dd8
mikihouse1023 <167415128+mikihouse1023@users.noreply.github.com>
2025-01-13 21:12:58 +0900

    Delete storage directory

--- storage/framework/views/2622b11205a4fbb1dc4e98e5fb88ed10.php
+++
@@ -1,9 +0,0 @@
-
-<?php $__env->startSection('content'); ?>
-  <div class="w-layout-blockcontainer container-31 w-container">
-    <h1 class="heading-18">登録完了</h1>
-    <p class="paragraph-2">ログイン画面でログインを行ってください。</p>
-    <a href="login" class="link-13">ログイン画面へ戻る</a>
-  </div>
-  <?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\7_PHP応用\ecommerce-app\resources\views/registrationcomplete.blade.php ENDPATH**/ ?>
