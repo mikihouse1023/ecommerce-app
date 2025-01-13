06b188c8c5a8b1812bd160fbd3b1be972e8407a4
mikihouse1023 <167415128+mikihouse1023@users.noreply.github.com>
2025-01-13 21:12:29 +0900

    Delete resources/views directory

--- resources/views/registrationcomplete.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-@section('content')
-  <div class="w-layout-blockcontainer container-31 w-container">
-    <h1 class="heading-18">登録完了</h1>
-    <p class="paragraph-2">ログイン画面でログインを行ってください。</p>
-    <a href="{{ route('login') }}" class="link-13">ログイン画面へ戻る</a>
-  </div>
-  @endsection
